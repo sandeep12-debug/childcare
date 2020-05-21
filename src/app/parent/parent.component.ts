335c0faee87c5b8276757a5c1bbefb21f24bc8b4
pjayneet97 <porwaljayneet97@gmail.com>
2020-05-21 10:54:08 +0530

    added updates

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth:AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    this.auth.logOut()
   }
 
 }
